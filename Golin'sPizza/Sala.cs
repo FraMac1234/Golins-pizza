ca1f04520b361926442ac3dabf1b2d0ba4eae922
S.H.E.E.P <147610524+FraMac1234@users.noreply.github.com>
2024-02-21 13:05:51 +0100

    Add files via upload

---
+++ Golin'sPizza/Sala.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Golin_sPizza
+{
+    internal class Sala
+    {
+        private Tavolo[] _listaTavoli;
+        private int _indice = 0;
+        private bool _allarme;
+        public Sala(int tTotali)
+        {
+            if (tTotali <= 0)
+                throw new Exception("La sala deve contenere almeno 1 tavolo");
+            _listaTavoli = new Tavolo[tTotali];
+            
+            
+        }
+
+        public int TavoliTotali
+        {
+            get
+            {
+                return _listaTavoli.Length; 
+            }
+        }
+
+        public int EliminaTavolo
+        {
+            set
+            {
+                _listaTavoli[_indice] = null;
+                _indice--;
+                ControlloAllarme(_indice);
+            }
+        }
+
+        public Tavolo AggiungiTavolo
+        {
+            set
+            {
+                ControlloAllarme(_indice);
+                if (_allarme == false)
+                {
+                    _listaTavoli[_indice] = value;
+                }
+            }
+        }
+
+        void ControlloAllarme(int tavoliOccupati)
+        {
+            if(tavoliOccupati > (TavoliTotali * 0.9))
+            {
+                _allarme = true;
+            }
+            else
+            {
+                _allarme = false;
+            }
+        }
+
+    }
+}
---
+++ Golin'sPizza/Totem.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Threading;
+using System.Net.NetworkInformation;
+
+namespace Golin_sPizza
+{
+    public class Totem
+    {
+        public double Prezzo{ get; set;}
+        public Totem(double prezzo) 
+        {
+            Prezzo = prezzo;
+        }
+
+        /// <summary>
+        /// 
+        /// </summary>
+        /// <returns></returns>
+        int CreazioneCoda()
+        {
+            Random random = new Random();
+            int attesa = random.Next(2, 5);
+            Console.WriteLine("Tempo di attesa in coda = " + attesa + "secondi.");
+            return attesa;
+        }
+
+    }
+}
